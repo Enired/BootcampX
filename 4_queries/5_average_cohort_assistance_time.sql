f1edb3cfdb66fb6f12b6634998768feb52271735
Derine Banis <d.banis.13@gmail.com>
2022-06-28 13:13:47 -0700

    Complete Q1-5 for assistance queries assignment

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as name, avg(assistance_requests.completed_at - assistance_requests.started_at) AS avg_assistance_time
+FROM cohorts
+JOIN students ON students.cohort_id = cohorts.id
+JOIN assistance_requests ON assistance_requests.student_id = students.id
+GROUP BY cohorts.name 
+ORDER BY avg_assistance_time
